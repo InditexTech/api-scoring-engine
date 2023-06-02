2eaeb065d73300b8146e040fc45595127605ee51
Pedro Gude Teira <pedrogt@ext.inditex.com>
2023-06-02 15:35:28 +0200

    feat: adapt to newest contrat
    update endpoints to /apis/

--- packages/certification-service/code/src/routes/validations/validationsRoutes.js
+++ packages/certification-service/code/src/routes/validations/validationsRoutes.js
@@ -3,8 +3,8 @@ const { timeout } = require("../../middleware/timeOut");
 
 const validationRoutes = (router, prefix) => {
   router
-    .post(`${prefix}/validations`, timeout(120000), getValidationController.validate)
-    .post(`${prefix}/file-verify-protocol`, timeout(120000), getValidationController.validateFile);
+    .post(`${prefix}/apis/verify`, timeout(120000), getValidationController.validate)
+    .post(`${prefix}/apis/validate`, timeout(120000), getValidationController.validateFile);
 };
 
 module.exports = { validationRoutes };
